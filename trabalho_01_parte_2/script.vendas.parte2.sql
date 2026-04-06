6d9bf4e41a6dba2b5f9413c0ae831aa634b32722
Gilasexe <guilherme.silva.anselmo@gmail.com>
2026-04-06 16:33:34 -0300

    pequenos ajustes

--- trabalho_01_parte_2/script.vendas.parte2.sql
+++ trabalho_01_parte_2/script.vendas.parte2.sql
@@ -109,7 +109,7 @@ CREATE TRIGGER tg_fluxo2
 AFTER
 UPDATE ON vendas FOR EACH ROW EXECUTE FUNCTION trg_fluxo2_update();
 -- ====================================================================================
--- 4. TESTES DE VALIDAÇÃO (Para incluir no PDF)
+-- 4. TESTES DE VALIDAÇÃO
 -- ====================================================================================
 -- TESTE DO FLUXO 1: Inserindo uma venda nova com status 'Completed' (Total > 300)
 -- Obs: Assumindo que os IDs de cliente e produto '1' existam gerados no seu script base. 
